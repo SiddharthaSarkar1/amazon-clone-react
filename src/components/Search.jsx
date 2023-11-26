1aad8e4ea202ae39877aa50bd69fdbe5fa45da8f
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2023-11-27 00:26:02 +0530

    worked on search box functionalities in navbar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useNavigate, createSearchParams } from 'react-router-dom';
 
 import { callAPI } from '../utils/CallApi'
 
@@ -7,7 +8,25 @@ const Search = () => {
 
   const [suggestions, setSuggestions] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
-  const [category, setCategory] = useState("All")
+  const [category, setCategory] = useState("All");
+
+  const navigate = useNavigate();
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate({
+      pathname: "search",
+      search: `${
+        createSearchParams({
+          category: `${category}`,
+          searchTerm: `${searchTerm}`
+        })
+      }`
+    })
+    setSearchTerm("");
+    setCategory("All");
+  }
 
   const getSuggestions = (params) => {
     callAPI("data/suggestions.json")
@@ -24,7 +43,7 @@ const Search = () => {
   return (
     <div className='w-[100%]'>
         <div className="flex items-center h-10 bg-amazonClone-yellow bg-[#FEBD69] rounded">
-            <select className='p-2 bg-gray-300 text-black border text-xs xl:text-sm'>
+            <select onChange={(e) => setCategory(e.target.value)} className='p-2 bg-gray-300 text-black border text-xs xl:text-sm'>
                 <option>All</option>
                 <option>Deals</option>
                 <option>Amazon</option>
@@ -34,7 +53,7 @@ const Search = () => {
                 <option>Mobiles</option>
             </select>
             <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className='flex grow items-center h-[100%] rounded-l text-black' />
-            <button className='w-[45px]'>
+            <button onClick={onHandleSubmit} className='w-[45px]'>
                 <MagnifyingGlassIcon className='h-[27px] m-auto stroke-slate-900' />
             </button>
         </div>

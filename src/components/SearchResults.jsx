778b4bcb207f59a9c70ef120e05d06f4d0d59994
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2023-11-28 00:29:52 +0530

    developed  search results component

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSearchParams, Link } from "react-router-dom";
+
+import { callAPI } from "../utils/CallApi";
+import { ProductDetails } from './';
+import { IN_CURRENCY } from '../utils/Constants';
 
 const SearchResults = () => {
-  return (
-    <div>SearchResults</div>
-  )
-}
+  const [searchParams] = useSearchParams();
+  const [products, setProducts] = useState(null);
 
-export default SearchResults
+  const getSearchResults = () => {
+    const searchTerm = searchParams.get("searchTerm");
+    const category = searchParams.get("category");
+
+    callAPI(`data/search.json`).then((searchResults) => {
+      const categoryResults = searchResults[category];
+      if (searchTerm) {
+        const results = categoryResults.filter((product) =>
+          product.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+        );
+        setProducts(results);
+      } else {
+        setProducts(categoryResults);
+      }
+    });
+  };
+
+  useEffect(() => {
+    getSearchResults();
+  }, [searchParams]);
+
+  return (
+    <div className="min-w-[1200px] max-w-[1300px] m-auto pt-4">
+      {products &&
+        products.map((product, key) => {
+          return (
+            <Link key={key} to={`/product/${product.id}`}>
+              <div className="h-[250px] grid grid-cols-12 rounded mt-1 mb-2">
+                <div className="col-span-2 p-4 bg-gray-200">
+                  <img src={product.image_small} className="m-auto" />
+                </div>
+                <div className="col-span-10 bg-gray-50 border-gray-100 hover:bg-gray-100">
+                  <div className="font-medium text-black p-2">
+                    <ProductDetails product={product} ratings={true} />
+                    <div className="text-xl xl:text-2xl pt-1">
+                      {IN_CURRENCY.format(product.price)}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+    </div>
+  );
+};
+
+export default SearchResults;
